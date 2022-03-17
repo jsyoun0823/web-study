9ed6e5835a98462aedef120566b7e200cd95a898
yjsun <yjsun_@naver.com>
2022-03-17 22:51:33 +0900

    오브젝트 Ch01 - 티켓 판매 어플리케이션

--- Ojbect/Ch01/Audience.java
+++ Ojbect/Ch01/Audience.java
@@ -5,7 +5,16 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+    // Bag의 존재를 내부로 캡슐화할 수 있다.
+
+    public Long buy(Ticket ticket) {
+        if(bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 }
--- Ojbect/Ch01/TicketSeller.java
+++ Ojbect/Ch01/TicketSeller.java
@@ -5,7 +5,10 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    // getTicketOffice 메서드를 제거함으로써, 외부에서는 ticketOffice에 직접 접근할 수 없다.
+    // 결과적으로, ticketOffice에 대한 접근은 오직 TicketSeller 안에만 존재하게 된다.
+
+    public void sellTo(Audience audience) {
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
